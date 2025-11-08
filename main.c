def3179f28440ed50c21d32e219d3f849c7bbdd7
Quentin Hoarau <quentin.hoarau@epitech.eu>
2025-11-08 21:59:33 +0100

    take board from file work

--- main.c
+++ main.c
@@ -7,19 +7,49 @@
 
 #include "my.h"
 
+char *erase_first_line(char *matrix)
+{
+    int count = 0;
+
+    for (int i = 0; matrix[i] != '\n'; i++) {
+        count++;
+    }
+    return (matrix + count + 1);
+}
+
 char *read_file(char *path)
 {
-    char *matrix;
+    int count = 0;
     char buff[SIZEBUFFER];
     int nb_bytes = 0;
     int fd = open(path, O_RDONLY);
+    char *matrix;
 
     if (fd == -1) {
         return FILE_ERROR;
     }
     nb_bytes = read(fd, buff, SIZEBUFFER);
     buff[nb_bytes + 1] = '\0';
-    matrix = my_strdup(buff);
+    matrix = my_strdup(erase_first_line(buff));
+    return matrix;
+}
+
+char *generate_matrix(int size, char *seed)
+{
+    char *matrix = malloc((size * size) + (size - 1) + 1);
+    int k = 0;
+
+    for (int i = 0; i < size; i++) {
+        for (int j = 0; j < size; j++) {
+            matrix[k] = '.';
+            k++;
+        }
+        if (i < size -1) {
+            matrix[k] = '\n';
+            k++;
+        }
+    }
+    matrix[k] = '\0';
     return matrix;
 }
 
@@ -27,6 +57,10 @@ int main(int ac, char **av)
 {
     char *matrix = malloc(SIZEBUFFER);
 
+    if (ac == 1) {
+        my_printf("To few arguments\n");
+        return 84;
+    }
     if (is_apha_char(av[1][0]) == 1) {
         //part with matrix from the file
         matrix = read_file(av[1]);
@@ -39,6 +73,13 @@ int main(int ac, char **av)
     }
     if (is_number(av[1][0]) == 1) {
         //part with the random generator of matrix
+        if (av[2] == NULL) {
+            my_printf("To few arguments\n");
+            return 84;
+        }
+        matrix = generate_matrix(my_atoi(av[1]),av[2]);
+        printf("%s", matrix);
     }
+    free(matrix);
     return 0;
 }
